352c893ba919503c81ce7ed7b6d95b166f22245d
Juan Calheiros <juan.calheiros@g.globo>
2021-10-26 22:01:01 -0300

    refactor: renamed variable

--- random_pior_caso.js
+++ random_pior_caso.js
@@ -1,13 +1,13 @@
 //node random_pior_caso.js -i turmas/123.json -o grupos -q 6 -s 1
 
 const argv = require('yargs/yargs')(process.argv.slice(2)).argv;
-const formarGrupos = require("./script").main
+const formGroups = require("./script").main
 const fs = require('fs');
 
 const rawdata = fs.readFileSync(argv.i);
-const turma = JSON.parse(rawdata);
-const groups = formarGrupos(turma, argv.q)
-const { hardskills_atividade } = turma
+const classroom = JSON.parse(rawdata);
+const groups = formGroups(classroom, argv.q)
+const { hardskills_atividade } = classroom
 const regexp = /[0-9]\d+/g
 const classNumber = `${regexp.exec(argv.i)[0]}_${argv.s}`
 const filePath = `${argv.o}/${classNumber}.json`
